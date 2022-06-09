572b158140228a7d87a777becd169f74c5f0d6af
mani703 <mani703@naver.com>
2022-06-09 12:53:14 +0900

    controller with api json

---
+++ spring03/src/main/java/com/bit/spring03/controller/ApiController.java
@@ -0,0 +1,42 @@
+package com.bit.spring03.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bit.spring03.domain.EmpDao;
+import com.bit.spring03.domain.EmpVo;
+
+@Controller
+@RequestMapping("/api")
+public class ApiController {
+	
+	@Autowired
+	EmpDao empDao;
+	
+	@RequestMapping(value = "/emp", method = RequestMethod.GET)
+	public void list(HttpServletResponse res) throws Exception {
+		res.setContentType("application/json; charset=utf-8");
+		PrintWriter out = res.getWriter();
+		out.print("{\"root\":[");
+		boolean boo = true;
+		for(EmpVo bean: empDao.findAll()) {
+			if(boo) {
+				boo = false;
+			} else {
+				out.print(',');
+			}
+			out.print("{\"empno\":" + bean.getEmpno() + ","
+					+ "\"ename\":\"" + bean.getEname() + "\","
+					+ "\"hiredate\":\"" 
+					+ (bean.getHiredate() == null ? "" : bean.getHiredate().getTime()) + "\"}");
+		}
+		out.print("]}");
+	}
+}
---
+++ spring03/src/main/java/com/bit/spring03/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.bit.spring03.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/list")
+	public void list() {
+		
+	}
+}

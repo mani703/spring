9c5f82785047e5a8f8b249b830c7d4b9f3efa23b
myeongmanblog <mani703@korea.ac.kr>
2022-06-10 11:02:38 +0900

    dao interface crud

---
+++ spring04/src/main/java/com/bit/spring04/domain/EmpDao.java
@@ -0,0 +1,14 @@
+package com.bit.spring04.domain;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface EmpDao {
+	
+	List<EmpVo> findAll() throws SQLException;
+	EmpVo findOne(int idx) throws SQLException;
+	void insertOne(EmpVo bean) throws SQLException;
+	int updateOne(EmpVo bean) throws SQLException;
+	int deleteOne(int idx) throws SQLException;
+	int allSize() throws SQLException;
+}

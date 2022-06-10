e54e46c5830d55b3a57aab128a2a764452ad7b42
myeongmanblog <mani703@korea.ac.kr>
2022-06-10 14:16:20 +0900

    Dao class Test

---
+++ spring04/src/test/java/com/bit/spring04/domain/EmpDaoTest.java
@@ -0,0 +1,79 @@
+package com.bit.spring04.domain;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class EmpDaoTest {
+	
+	static EmpDao empDao;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		ApplicationContext ac=null;
+//		new ClassPathXmlApplicationContext("/application-context.xml");
+//		new FileSystemXmlApplicationContext("c:\\~~\\sts07/src/main/webapp/WEB-INF/spring/root-context.xml");
+//		new FileSystemXmlApplicationContext("./src/main/webapp/WEB-INF/spring/root-context.xml");
+//		ac=new GenericXmlApplicationContext("classpath:/WEB-INF/spring/root-context.xml");
+		ac=new GenericXmlApplicationContext("file:src/main/webapp/WEB-INF/spring/root-context.xml");
+		empDao=ac.getBean(EmpDao.class);
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+	
+	@Test
+	public void test1DataSource() throws BeansException, SQLException {
+		ApplicationContext ac=null;
+		ac=new GenericXmlApplicationContext("file:src/main/webapp/WEB-INF/spring/root-context.xml");
+		ac.getBean(DataSource.class).getConnection().close();
+	}
+
+	@Test
+	public void test2FindAll() throws SQLException {
+		assertNotNull(empDao);
+		assertNotNull(empDao.findAll());
+		assertNotEquals(0,empDao.findAll().size());
+	}
+//
+//	@Test
+//	public void testFindOne() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testInsertOne() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testUpdateOne() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testDeleteOne() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testAllSize() {
+//		fail("Not yet implemented");
+//	}
+
+}

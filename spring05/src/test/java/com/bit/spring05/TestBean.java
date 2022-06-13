a309374e11bb475cd29128fb51094c6d3379678d
mani703 <mani703@naver.com>
2022-06-13 10:11:05 +0900

    project & mybatis init

---
+++ spring05/src/test/java/com/bit/spring05/TestBean.java
@@ -0,0 +1,28 @@
+package com.bit.spring05;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
+public class TestBean {
+	
+	@Autowired
+	DataSource dataSource;
+
+	@Test
+	public void test() throws SQLException {
+		assertNotNull(dataSource);
+		dataSource.getConnection().close();
+	}
+
+}

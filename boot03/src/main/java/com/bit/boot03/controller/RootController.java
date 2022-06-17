0f21f2176fada929b2d53e4972ab2c39a04ea33b
mani703 <mani703@naver.com>
2022-06-17 10:05:24 +0900

    spring boot project03

---
+++ boot03/src/main/java/com/bit/boot03/controller/RootController.java
@@ -0,0 +1,22 @@
+package com.bit.boot03.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class RootController {
+
+	@GetMapping("/")
+	@ResponseBody
+	public String index() {
+		return "Hello world";
+	}
+	
+	@GetMapping("/ex01")
+	public String ex01(Model model) {
+		model.addAttribute("msg", "환영합니다.");
+		return "ex01";
+	}
+}

ffef8d11af581feb1791f686bba9c719fe848908
mani703 <mani703@naver.com>
2022-06-20 11:03:13 +0900

    mybatis mapper

---
+++ boot04/src/main/java/com/bit/boot04/mapper/DeptMapper.java
@@ -0,0 +1,31 @@
+package com.bit.boot04.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.bit.boot04.mapper.domain.Dept;
+
+@Mapper
+public interface DeptMapper {
+	
+	@Select("select * from dept")
+	List<Dept> findAll();
+	
+	@Select("select * from dept where deptno=#{deptno}")
+	Dept findOne(int deptno);
+	
+	@Insert("insert into dept (deptno, dname, loc) values (#{deptno}, #{dname}, #{loc})")
+	int insertOne(Dept dept);
+	
+	@Update("update dept set dname=#{dname}, loc=#{loc} where deptno=#{deptno}")
+	int updateOne(Dept dept);
+	
+	@Delete("delete from dept where deptno=#{deptno}")
+	int deleteOne(int deptno);
+	
+}

63b2b8e70926b3d62451681f29e3c6072513656a
mani703 <mani703@naver.com>
2022-06-24 12:55:51 +0900

    api crud service

---
+++ boot08/src/main/java/com/bit/boot08/domain/Emp.java
@@ -0,0 +1,17 @@
+package com.bit.boot08.domain;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Emp {
+	
+	private int sabun, pay;
+	private String ename;
+	private Date nalja;
+}

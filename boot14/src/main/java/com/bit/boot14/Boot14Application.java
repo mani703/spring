6a2a078750bd2e434fc7c92dc5a3e471ac7535fe
mani703 <mani703@naver.com>
2022-06-28 09:08:04 +0900

    jwt

---
+++ boot14/src/main/java/com/bit/boot14/Boot14Application.java
@@ -0,0 +1,13 @@
+package com.bit.boot14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot14Application.class, args);
+	}
+
+}
